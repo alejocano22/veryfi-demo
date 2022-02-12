fba16528ebaca07c15004bbb774b4fb68e7bc2f0
Alejandro Cano <alejocano22@gmail.com>
2022-02-12 15:04:37 -0500

    add home and login page mock

--- src/screens/login/LoginScreen.tsx
+++ src/screens/login/LoginScreen.tsx
@@ -8,15 +8,40 @@ export default function LoginScreen() {
   const { push: routerPush, locale } = useRouter();
   const { title, go } = i18nLogin[locale];
   return (
-    <div className='h-screen flex flex-col items-center justify-center '>
-      <h1 className='text-xl mb-5'>{title}</h1>
-      <button
-        className='border-2 rounded-xl border-green-500 p-4 cursor-pointer hover:bg-green-100'
-        onClick={() => routerPush('/')}
-      >
-        {go}
-      </button>
-      <Language />
+    <div className='w-full min-h-screen bg-gray-50 flex flex-col sm:justify-center items-center pt-6 sm:pt-0'>
+      <div className='w-full sm:max-w-md p-5 mx-auto'>
+        <h2 className='mb-12 text-center text-5xl font-extrabold'>Welcome.</h2>
+        <form>
+          <div className='mb-4'>
+            <label className='block mb-1' id='email'>
+              Email
+            </label>
+            <input
+              id='email'
+              type='text'
+              name='email'
+              className='py-2 px-3 border border-gray-300 focus:border-red-300 focus:outline-none focus:ring focus:ring-red-200 focus:ring-opacity-50 rounded-md shadow-sm disabled:bg-gray-100 mt-1 block w-full'
+            />
+          </div>
+          <div className='mb-4'>
+            <label className='block mb-1' id='password'>
+              Password
+            </label>
+            <input
+              id='password'
+              type='password'
+              name='password'
+              className='py-2 px-3 border border-gray-300 focus:border-red-300 focus:outline-none focus:ring focus:ring-red-200 focus:ring-opacity-50 rounded-md shadow-sm disabled:bg-gray-100 mt-1 block w-full'
+            />
+          </div>
+          <div className='mt-6 flex items-center justify-between'></div>
+          <div className='mt-6'>
+            <button className='w-full inline-flex items-center justify-center px-4 py-2 bg-purple-900 border border-transparent rounded-md font-semibold capitalize text-white hover:bg-purple-700 active:bg-purple-700 focus:outline-none focus:border-red-700 focus:ring focus:ring-red-200 disabled:opacity-25 transition'>
+              Sign In
+            </button>
+          </div>
+        </form>
+      </div>
     </div>
   );
 }
