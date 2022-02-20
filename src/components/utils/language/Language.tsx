6e774e6460432f670cdc2926f92fe505d4aa2b7e
Alejandro Cano <alejocano22@gmail.com>
2022-02-20 13:54:51 -0500

    fix language design

--- src/components/utils/language/Language.tsx
+++ src/components/utils/language/Language.tsx
@@ -1,27 +1,20 @@
 import Link from 'next/link';
 import { useRouter } from 'next/router';
-import { i18nLogin } from '../../../i18n/login';
+import { GlobeAltIcon } from '@heroicons/react/outline';
 
 export interface LanguageProps {}
 
 export default function Language() {
   const { locale, asPath } = useRouter();
-  const { title } = i18nLogin[locale];
+
   return (
-    <>
-      {locale === 'en' ? (
-        <Link href={asPath} locale={'es'}>
-          <a className='block px-3 py-2 rounded-md text-base font-medium text-white hover:text-gray-light'>
-            {'ES'}
-          </a>
-        </Link>
-      ) : (
-        <Link href={asPath} locale={'en'}>
-          <a className='block px-3 py-2 rounded-md text-base font-medium text-white hover:text-gray-light'>
-            {'EN'}
-          </a>
-        </Link>
-      )}
-    </>
+    <div className='flex items-center px-3 '>
+      <GlobeAltIcon className='mr-1 ' height={20} width={20} color='white' />
+      <Link href={asPath} locale={locale === 'en' ? 'es' : 'en'}>
+        <a className='text-white hover:text-gray-light'>
+          {locale === 'en' ? 'EN' : 'ES'}
+        </a>
+      </Link>
+    </div>
   );
 }
